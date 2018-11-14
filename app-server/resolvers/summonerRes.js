e90c2a31a71cb1499774f52f11e8239dda96de16
satner <cse2343@gmail.com>
2018-11-14 20:04:33 +0200

    MongoDB: check if user exist

--- app-server/resolvers/summonerRes.js
+++ app-server/resolvers/summonerRes.js
@@ -8,20 +8,20 @@ export default {
     },
     Mutation: {
         setSummonerInfo: async (_source, _args, {dataSources}) => {
-            SummonerSchema.findOne({name: _args.summonerName}, (err, user) => {
+
+            SummonerSchema.findOne({name: _args.summonerName}, async (err, user) => {
                 if (user) {
-                    return false
+                    console.log('Summoner exist!')
+                } else {
+                    console.log('Summoner does not exist');
+                    let data = await dataSources.summonerDataSource.getSummoner(_args.summonerName);
+                    let data2 = await dataSources.leagueDataSource.getLeagueRank(data.id);
+                    let data3 = await dataSources.matchDataSource.getMatches(data.accountId);
+                    let finalData = Object.assign({}, data, data2[1], data3);
+                    SummonerSchema.create(finalData);
                 }
+                return true
             });
-            // TODO: na ginete check ean to summoner name iparxei idi sto database
-            // TODO: na ginete store to summoner name olo lower case
-            let data = await dataSources.summonerDataSource.getSummoner(_args.summonerName);
-            let data2 = await dataSources.leagueDataSource.getLeagueRank(data.id);
-            let data3 = await dataSources.matchDataSource.getMatches(data.accountId);
-            let finalData = Object.assign({}, data, data2[1], data3);
-            console.log(finalData);
-            SummonerSchema.create(finalData);
-            return true
         },
     }
 }
