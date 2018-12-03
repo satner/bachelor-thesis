467c2dd4d12f7dd632216468691d7dd924af1376
satner <cse2343@gmail.com>
2018-12-03 16:07:38 +0200

    Remake front page style

--- app-client/teamate-stats/src/App.js
+++ app-client/teamate-stats/src/App.js
@@ -7,37 +7,61 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <div className='container' style={container}>
-                    <div>
-                        <h1 style={{fontFamily: 'Muli', color: 'White'}}>Team Mate Stats</h1>
-                    </div>
-                    <div>
-                        <Button ghost style={btnContainer}>Log in</Button>
-                        <Button ghost style={btnContainer}>Sign up</Button>
-                    </div>
-                </div>
-                <hr/>
-                <img src={require('./images/Layer 1.svg')} alt='Background' style={{position: 'absolute', top: '0', zIndex: '-1', left: '0'}}/>
 
-                <div id="main-pitch" style={pitch}>
+                <header className='site-header'>
+                    <div className='container'>
+                        <nav className='nav nav--inline'>
+                            <a style={{fontFamily: 'Muli', color: 'White', fontSize: '30px'}} href='/'>Team Mate Stats</a>
+                            <div>
+                                <Button ghost style={btnContainer}>Log in</Button>
+                                <Button ghost style={btnContainer}>Sign up</Button>
+                            </div>
+                        </nav>
+                    </div>
+                </header>
+
+                <hr/>
+
+                <main id='main'>
+                    <section className='summoner summoner--home'>
+                        <div className="container">
+                            <h1>
+                                <span>Build, </span>
+                                <span>deploy, </span>
+                                <span>and manage </span>
+                                <span className="smaller">modern web projects</span>
+                            </h1>
+                            <p>
+                                An all-in-one workflow that combines global deployment,
+                                continuous integration, and automatic HTTPS. And that’s just the beginning.
+                            </p>
+                            <footer>
+                                <p>
+                                    <a className="button" href="https://app.netlify.com/signup"
+                                       id="cta-summoner-getstartedforfree">Get started for free</a>
+                                </p>
+                            </footer>
+                        </div>
+                        <div className="illo">
+                            {/*<img src={require('./images/Layer 1.svg')} alt='Background' style={{position: 'absolute', top: '0', zIndex: '-100', left: '0'}}/>*/}
+                            <img src={require('./images/final-bg.svg')} alt='Background'/>
+                        </div>
+                    </section>
+                    <section></section>
+                    <section></section>
+
+                </main>
+
+
+               {/* <section id="main-pitch" style={pitch}>
                     <h1 style={{fontFamily: 'Muli', color: 'White'}}>Search the ideal teammate</h1>
                     <p style={{fontFamily: 'Muli', color: 'White'}}>Became a member of healthy league of legends community</p>
                     <Button size={'large'}>Get started for free</Button>
-                </div>
+                </section>*/}
             </div>
         );
     }
 }
-
-const container = {
-    display: 'flex',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'space-between',
-    paddingTop: '.618em',
-    maxWidth: '60rem',
-    margin: '0 auto',
-};
 
 const btnContainer = {
     marginLeft: '1em'
