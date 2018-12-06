9b6d4cb3f786e4131d9488675363592833d86fc0
satner <cse2343@gmail.com>
2018-12-07 01:31:21 +0200

    Refactor Summoners file

--- app-client/teamate-stats/src/App.js
+++ app-client/teamate-stats/src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import ApolloClient from 'apollo-boost';
-import {ApolloProvider, Query} from "react-apollo";
+import {ApolloProvider} from "react-apollo";
 
 import './App.css';
 import Navigation from './components/Navigation';
@@ -11,44 +11,10 @@ import Footer from './components/Footer';
 import LogIn from './components/LogIn';
 import SignUp from './components/SignUp';
 import Error from './components/Error';
-import gql from "graphql-tag";
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/graphql"
 })
-
-
-const ExchangeRates = () => (
-    <Query
-        query={gql`
-      query {
-  getAllUsers {
-    summoner
-    server
-    languages {
-      lang
-    }
-  }
-}
-    `}
-        errorPolicy="all"
-    >
-      {({ loading, error, data }) => {
-        if (loading) return <p>Loading...</p>;
-
-        return (
-            <div style={{backgroundColor: 'yellow'}}>
-
-              {
-                data.getAllUsers.forEach(d => {
-                  return <h1 style={{backgroundColor: 'yellow'}} >{d.summoner }</h1>
-                })
-              }
-            </div>
-        );
-      }}
-    </Query>
-);
 
 // TODO: override ant-design font-family
 class App extends Component {
--- app-client/teamate-stats/src/components/Summoners.js
+++ app-client/teamate-stats/src/components/Summoners.js
@@ -1,195 +1,174 @@
 import React, {Component} from 'react';
-import { Select, Checkbox, Card, Icon, Avatar, Badge, Skeleton } from 'antd';
-import gql from 'graphql-tag'
-import { Query } from "react-apollo";
-import leaguejs from 'leaguejs'
+import { Select, Checkbox, Badge } from 'antd';
 import lang from '../languages-v2'
+import SummonerCard from './SummonerCard'
 
-const { Meta } = Card;
 const Option = Select.Option;
 const CheckboxGroup = Checkbox.Group;
-
-const ExchangeRates = () => (
-    <Query
-        query={gql`
-      query {
-  getAllUsers {
-    summoner
-    server
-    languages {
-      lang
-    }
-  }
-}
-    `}
-        errorPolicy="all"
-    >
-        {({ loading, error, data }) => {
-            if (loading) return <Skeleton avatar active paragraph={{ rows: 4 , width: '200px'}} />;
-
-            return (
-
-                    <Card
-                        hoverable={true}
-                        className={'summoner-card'}
-                        style={{ width: 300 }}
-                        cover={<img alt="example" src=`` />}
-                        actions={[<Icon type="setting" />, <Icon type="edit" />, <Icon type="ellipsis" />]}
-                    >
-                        <Meta
-                            avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
-                            title={data.getAllUsers[0].summoner}
-                            description="This is the description"
-                        />
-                    </Card>
-            );
-        }}
-    </Query>
-);
+const tiers = [
+  {name: 'provisional', path: require('../images/tier-icons/provisional.png')},
+  {name: 'bronze', path: require('../images/tier-icons/bronze.png')},
+  {name: 'silver', path: require('../images/tier-icons/silver.png')},
+  {name: 'gold', path: require('../images/tier-icons/gold.png')},
+  {name: 'platinum', path: require('../images/tier-icons/platinum.png')},
+  {name: 'diamond', path: require('../images/tier-icons/diamond.png')},
+  {name: 'master', path: require('../images/tier-icons/master.png')},
+  {name: 'challenger', path: require('../images/tier-icons/challenger.png')},
+];
+const roles = [
+  {name: 'bottom', path: require('../images/role-icons/Bottom_icon.png')},
+  {name: 'support', path: require('../images/role-icons/Support_icon.png')},
+  {name: 'middle', path: require('../images/role-icons/Middle_icon.png')},
+  {name: 'jungle', path: require('../images/role-icons/Jungle_icon.png')},
+  {name: 'top', path: require('../images/role-icons/Top_icon.png')},
+  {name: 'specialist', path: require('../images/role-icons/Specialist_icon.png')},
+];
+const servers = [
+  {shortName: 'na', name: 'North America'},
+  {shortName: 'kr', name: 'Republic of korea'},
+  {shortName: 'ru', name: 'Russia'},
+  {shortName: 'br1', name: 'Brazil'},
+  {shortName: 'eun1', name: 'Europe Nordic and East'},
+  {shortName: 'euw1', name: 'Europe West'},
+  {shortName: 'jp1', name: 'Japan'},
+  {shortName: 'la1', name: 'Latin America North'},
+  {shortName: 'la2', name: 'Latin America South'},
+  {shortName: 'oc1', name: 'Ocean'},
+  {shortName: 'tr1', name: 'Turkey'},
+];
 
 class Summoners extends Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            listTier: [],
-            listRoles: [],
-            listLanguages: [],
-            server: '',
-            badgeTier: 0,
-            badgeLanguage: 0
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      listTier: [],
+      listRoles: [],
+      listLanguages: [],
+      server: '',
+      badgeTier: 0,
+      badgeLanguage: 0
     }
+  }
 
-    handleChangeTier = listTier => {
-        let badgeTier = listTier.length;
-        this.setState({
-            listTier,
-            badgeTier
-        });
-    }
+  handleChangeTier = listTier => {
+    let badgeTier = listTier.length;
+    this.setState({
+      listTier,
+      badgeTier
+    });
+    console.log(this.state.listTier)
+  };
 
-    handleChangeRoles = (listRoles) => {
-        this.setState({
-            listRoles
-        });
-    }
+  handleChangeRoles = (listRoles) => {
+    this.setState({
+      listRoles
+    });
+    console.log(this.state.listRoles)
+  };
 
-    handleChangeServer = server => {
-        this.setState({
-        server
-        })
-    }
+  handleChangeServer = server => {
+    this.setState({
+      server
+    });
+    console.log(this.state.server)
+  };
 
-    handleChangeLanguage = listLanguages => {
-        let badgeLanguage = listLanguages.length
-        this.setState({
-            listLanguages,
-            badgeLanguage
-        })
-    }
+  handleChangeLanguage = listLanguages => {
+    let badgeLanguage = listLanguages.length
+    this.setState({
+      listLanguages,
+      badgeLanguage
+    });
+    console.log(this.state.listLanguages)
+  };
 
-    render() {
-        let langHTML = []
-        lang.forEach(l => {
-            langHTML.push(<Option key={l.code}>{l.name}</Option>)
-        })
+  render() {
+    return (
+        <main id="main" className={'test'}>
+          <div className="illo" style={{position: 'absolute', top: '0', zIndex: '-1', width: '100%'}}>
+            <img src={require('../images/wave.svg')} alt='Background'/>
+          </div>
 
-        return (
-            <main id="main" className={'test'}>
-                <div className="illo" style={{position: 'absolute', top: '0', zIndex: '-1', width: '100%'}}>
-                    <img src={require('../images/wave.svg')} alt='Background'/>
-                </div>
+          <div className="summoners--container">
+            {/* All the filters div*/}
+            <div id='filters' style={{ marginTop: '200px', marginLeft: '50px' }}>
 
-                <div className="summoners--container">
+              {/* Filter selection of tier */}
+              <Badge count={this.state.badgeTier} offset={[10, 0]} title={'Number of selected tiers'}>
+                <h1>Select tier</h1>
+              </Badge>
+              <Select
+                  mode="multiple"
+                  style={{ width: '320px' }}
+                  placeholder="Select tier"
+                  size={'large'}
+                  showArrow={true}
+                  onChange={this.handleChangeTier}
+              >
+                {
+                  tiers.map(t => {
+                    return <Option key={t.name}><img src={t.path} style={{height: '25px', width: '30px'}} alt={t.name}/>{t.name.charAt(0).toUpperCase() + t.name.slice(1)}</Option>
+                  })
+                }
+              </Select>
 
+              {/* Filter selection of roles */}
+              <h1>Select Roles</h1>
+              <CheckboxGroup onChange={this.handleChangeRoles} >
+                {
+                  roles.map(r => {
+                    return [
+                      <Checkbox key={r.name} value={r.name}><img src={r.path} style={{height: '30px', width: '30px'}} alt={r.name}/>{r.name.charAt(0).toUpperCase() + r.name.slice(1)}</Checkbox>, <br key={r.name + 1} />
+                    ]
+                  })
+                }
+              </CheckboxGroup>
 
-                    {/* All the filters div*/}
-                    <div id='filters' style={{ marginTop: '200px', marginLeft: '50px' }}>
-                        {/* Filter selection of tier */}
-                        <Badge count={this.state.badgeTier} offset={[10, 0]} title={'Number of selected tiers'}>
-                            <h1>Select tier</h1>
-                        </Badge>
+              {/* Filter selection of server */}
+              <h1>Server Regions</h1>
+              <Select
+                  style={{ width: '320px' }}
+                  size={'large'}
+                  placeholder="Any"
+                  allowClear={true}
+                  onChange={this.handleChangeServer}>
+                {
+                  servers.map(s => {
+                    return <Option key={s.shortName} value={s.shortName}>{s.name}</Option>
+                  })
+                }
+              </Select>
 
-                        <Select
-                            mode="multiple"
-                            style={{ width: '320px' }}
-                            placeholder="Select tier"
-                            size={'large'}
-                            showArrow={true}
-                            onChange={this.handleChangeTier}
-                        >
-                            <Option key={'provisional'}><img src={require('../images/tier-icons/provisional.png')} style={{height: '25px', width: '30px'}} alt=""/> Provisional</Option>
-                            <Option key={'bronze'}><img src={require('../images/tier-icons/bronze.png')} style={{height: '25px', width: '30px'}} alt=""/> Bronze</Option>
-                            <Option key={'silver'}><img src={require('../images/tier-icons/silver.png')} style={{height: '25px', width: '30px'}} alt=""/> Silver</Option>
-                            <Option key={'gold'}><img src={require('../images/tier-icons/gold.png')} style={{height: '25px', width: '30px'}} alt=""/> Gold</Option>
-                            <Option key={'platinum'}><img src={require('../images/tier-icons/platinum.png')} style={{height: '25px', width: '30px'}} alt=""/> Platinum</Option>
-                            <Option key={'diamond'}><img src={require('../images/tier-icons/diamond.png')} style={{height: '25px', width: '30px'}} alt=""/> Diamond</Option>
-                            <Option key={'master'}><img src={require('../images/tier-icons/master.png')} style={{height: '25px', width: '30px'}} alt=""/> Master</Option>
-                            <Option key={'challenger'}><img src={require('../images/tier-icons/challenger.png')} style={{height: '25px', width: '30px'}} alt=""/> Challenger</Option>
-                        </Select>
+              {/* Filter selection of languages */}
+              <Badge count={this.state.badgeLanguage} offset={[10, 0]} title={'Number of selected languages'}>
+                <h1>Languages</h1>
+              </Badge>
+              <Select
+                  mode="multiple"
+                  style={{ width: '320px' }}
+                  placeholder="Any"
+                  size={'large'}
+                  onChange={this.handleChangeLanguage}
+              >
+                {
+                  lang.map(l => {
+                    return <Option key={l.code} value={l.code}>{l.name}</Option>
+                  })
+                }
+              </Select>
+            </div>
 
-                        {/* Filter selection of roles */}
-                        <h1>Select Roles</h1>
-                        <CheckboxGroup onChange={this.handleChangeRoles} >
-                            <Checkbox value={'bottom'}><img src={require('../images/role-icons/Bottom_icon.png')} style={{height: '30px', width: '30px'}} alt='Bottom role'/></Checkbox>
-                            <br />
-                            <Checkbox value={'support'}><img src={require('../images/role-icons/Support_icon.png')} style={{height: '30px', width: '30px'}} alt='Support role'/></Checkbox>
-                            <br />
-                            <Checkbox value={'middle'}><img src={require('../images/role-icons/Middle_icon.png')} style={{height: '30px', width: '30px'}} alt='Middle role'/></Checkbox>
-                            <br />
-                            <Checkbox value={'jungle'}><img src={require('../images/role-icons/Jungle_icon.png')} style={{height: '30px', width: '30px'}} alt='Jungle role'/></Checkbox>
-                            <br />
-                            <Checkbox value={'top'}><img src={require('../images/role-icons/Top_icon.png')} style={{height: '30px', width: '30px'}} alt='Top role'/></Checkbox>
-                            <br />
-                            <Checkbox value={'specialist'}><img src={require('../images/role-icons/Specialist_icon.png')} style={{height: '30px', width: '30px'}} alt='Specialist role'/></Checkbox>
-                            <br />
-                        </CheckboxGroup>
+            {/* Grid card layout of summoners*/}
+            <div className="summoners--grid" style={{ marginTop: '200px', marginLeft: '50px' }}>
+              <div className="card--grid">
+                <SummonerCard />
+              </div>
+            </div>
 
-                        {/* Filter selection of server */}
-                        <h1>Server Regions</h1>
-                        <Select
-                            style={{ width: '320px' }}
-                            size={'large'}
-                            placeholder="Any"
-                            allowClear={true}
-                            onChange={this.handleChangeServer}>
-                            <Option value='na'>North America</Option>
-                            <Option value='kr'>Republic of korea</Option>
-                            <Option value='ru'>Russia</Option>
-                            <Option value='br1'>Brazil</Option>
-                            <Option value='eun1'>Europe Nordic and East</Option>
-                            <Option value='euw1'>Europe West</Option>
-                            <Option value='jp1'>Japan</Option>
-                            <Option value='la1'>Latin America North</Option>
-                            <Option value='la2'>Latin America South</Option>
-                            <Option value='oc1'>Ocean</Option>
-                            <Option value='tr1'>Turkey</Option>
-                        </Select>
-
-                        {/* Filter selection of languages */}
-                        <Badge count={this.state.badgeLanguage} offset={[10, 0]} title={'Number of selected languages'}>
-                            <h1>Languages</h1>
-                        </Badge>
-                        <Select
-                            mode="multiple"
-                            style={{ width: '320px' }}
-                            placeholder="Any"
-                            size={'large'}
-                            onChange={this.handleChangeLanguage}
-                        >
-                            {langHTML}
-                        </Select>
-                    </div>
-                    {/* Grid card layout of summoners*/}
-                    <div className="summoners--grid" style={{ marginTop: '200px', marginLeft: '50px' }}>
-                        <div className="card--grid">
-                            <ExchangeRates />
-                        </div>
-                    </div>
-                </div>
-            </main>
-        );
-    }
+          </div>
+        </main>
+    );
+  }
 }
 
 export default Summoners;
